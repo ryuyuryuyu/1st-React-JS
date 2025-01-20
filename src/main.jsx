f3d49d2181c86f9ba8241ece2164a0f564360edd
ryuyuryuyu <nozaru.bosuzaru.hekokizaru@gmail.com>
2025-01-20 18:58:37 +0900

    コンポーネント化

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { App } from './App';
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-// 関数で画面を追加していく
-const App = () => {
-  return (
-    // h1,pタグだけではエラーになる。空タグ「<>」で囲ってあげる。（React.Fragmentタグ、Divタグで囲っていたこともある）
-    <>
-      <h1>こんにちは</h1>
-      <p>お元気ですか？</p>
-    </>
-  );
-};
-
-// Strictmode：厳密に。エラーが出るようになる。
 root.render(
   <StrictMode>
     <App />
