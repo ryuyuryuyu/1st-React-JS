1a453e077c7ba789ee976bc90ebd8dd6eb263cca
ryuyuryuyu <nozaru.bosuzaru.hekokizaru@gmail.com>
2025-01-21 18:30:03 +0900

    Reactでのイベントやスタイルの扱い方

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 // 拡張子「.jsx」：Reactのコンポーネントの意味を指す。
  export const App = () => {
+
+  // 本関数内で使う関数をここで定義する
+  const onClickButton = () => alert();
+
+  // cssのオブジェクト
+  const contentStyle = {
+    color: "blue",
+    fontSize: "36px",
+    margin: 10,
+  };
+
+  // {} はJavaScriptで書くよ、という宣言。
+  // アロー関数だった部分を上記に切り出した。この方が見やすい。
   return (
     <>
-      <h1>こんにちは</h1>
+      <h1 style={contentStyle}>こんにちは</h1>
       <p>お元気ですか？</p>
       {console.log('hoge!')}
-      <button onClick={() => alert()}>ボタン</button>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
