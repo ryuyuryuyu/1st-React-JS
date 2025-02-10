497a3dd019d2ad5d1bf44ae6e9b488fd02968612
ryuyuryuyu <nozaru.bosuzaru.hekokizaru@gmail.com>
2025-02-10 09:26:34 +0900

    引数で分割代入を使う方法

--- src/componets/ColorfulMessage.jsx
+++ src/componets/ColorfulMessage.jsx
@@ -1,9 +1,6 @@
 // コンポーネント化
-// 関数内で「props」を書きたくないとき
-export const ColorfulMessage = (props) => {
-    // 分割代入して変数で受け取る
-    const { color, children } = props;
-
+// 引数で分割代入を使う方法
+export const ColorfulMessage = ({ color, children }) => {
     // cssのオブジェクト
     const contentStyleA = {
         // プロパティと引数名が同名なら省略できので
@@ -17,6 +14,25 @@ export const ColorfulMessage = (props) => {
         <p style={contentStyleA}>{children}</p>
     )    
 }
+
+// // 関数内で「props」を書きたくないとき
+// export const ColorfulMessage = (props) => {
+//     // 分割代入して変数で受け取る
+//     const { color, children } = props;
+
+//     // cssのオブジェクト
+//     const contentStyleA = {
+//         // プロパティと引数名が同名なら省略できので
+//         color,
+//         //color: color,
+//         fontSize: "18px",
+//         margin: 10,
+//     };
+
+//     return (
+//         <p style={contentStyleA}>{children}</p>
+//     )    
+// }
 
 // // 普通のpタグと同じような使い方に変形
 // export const ColorfulMessage = (props) => {
