2b502ea3839ace3852bc05a26e632646ed3c13e4
ryuyuryuyu <nozaru.bosuzaru.hekokizaru@gmail.com>
2025-02-10 09:19:43 +0900

    propsを使っての引数

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,13 @@ import { ColorfulMessage } from "./componets/ColorfulMessage";
   // 本関数内で使う関数をここで定義する
   const onClickButton = () => alert();
 
-
-
   // {} はJavaScriptで書くよ、という宣言。
   // アロー関数だった部分を上記に切り出した。この方が見やすい。
   return (
     <>
       <h1 style={{ color: "red"}}>こんにちは</h1>
-      <ColorfulMessage color="green">"お元気ですか？"</ColorfulMessage>
-      <ColorfulMessage color="orange">"元気です"</ColorfulMessage>
+      <ColorfulMessage color="green">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="orange">元気です</ColorfulMessage>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
--- src/componets/ColorfulMessage.jsx
+++ src/componets/ColorfulMessage.jsx
@@ -1,12 +1,29 @@
+// コンポーネント化
+// 普通のpタグと同じような使い方に変形
 export const ColorfulMessage = (props) => {
-      // cssのオブジェクト
-    const contentStyleA = {
-    color: props.color,
-    fontSize: "18px",
-    margin: 10,
-    };
+    // cssのオブジェクト
+  const contentStyleA = {
+      color: props.color,
+      fontSize: "18px",
+      margin: 10,
+  };
 
-    return (
-        <p style={contentStyleA}>{props.children}</p>
-    )
+  return (
+      <p style={contentStyleA}>{props.children}</p>
+  )    
 }
+
+// <ColorfulMessage color="green" message="お元気ですか？"/>
+//  という呼び出し方法は以下。
+// export const ColorfulMessage = (props) => {
+//     // cssのオブジェクト
+//   const contentStyleA = {
+//       color: props.color,
+//       fontSize: "18px",
+//       margin: 10,
+//   };
+
+//   return (
+//       <p style={contentStyleA}>{props.message}</p>
+//   )    
+// }
