9659de8b9712ead818cb5122465312901761c97f
ryuyuryuyu <nozaru.bosuzaru.hekokizaru@gmail.com>
2025-02-10 09:25:44 +0900

    propsを分割代入で変数にわける方法

--- src/componets/ColorfulMessage.jsx
+++ src/componets/ColorfulMessage.jsx
@@ -1,17 +1,36 @@
 // コンポーネント化
-// 普通のpタグと同じような使い方に変形
+// 関数内で「props」を書きたくないとき
 export const ColorfulMessage = (props) => {
-    // cssのオブジェクト
-  const contentStyleA = {
-      color: props.color,
-      fontSize: "18px",
-      margin: 10,
-  };
+    // 分割代入して変数で受け取る
+    const { color, children } = props;
 
-  return (
-      <p style={contentStyleA}>{props.children}</p>
-  )    
+    // cssのオブジェクト
+    const contentStyleA = {
+        // プロパティと引数名が同名なら省略できので
+        color,
+        //color: color,
+        fontSize: "18px",
+        margin: 10,
+    };
+
+    return (
+        <p style={contentStyleA}>{children}</p>
+    )    
 }
+
+// // 普通のpタグと同じような使い方に変形
+// export const ColorfulMessage = (props) => {
+//     // cssのオブジェクト
+//   const contentStyleA = {
+//       color: props.color,
+//       fontSize: "18px",
+//       margin: 10,
+//   };
+
+//   return (
+//       <p style={contentStyleA}>{props.children}</p>
+//   )    
+// }
 
 // <ColorfulMessage color="green" message="お元気ですか？"/>
 //  という呼び出し方法は以下。
